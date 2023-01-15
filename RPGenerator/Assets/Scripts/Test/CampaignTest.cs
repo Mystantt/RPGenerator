d95abe4949170c21450b1456f75bf299a2b2446e
Mystantt <dompeydnf@gmail.com>
2023-01-15 15:55:18 +0100

    little correction

--- RPGenerator/Assets/Scripts/Test/CampaignTest.cs
+++ RPGenerator/Assets/Scripts/Test/CampaignTest.cs
@@ -13,6 +13,7 @@ namespace Assets.Scripts.Test
         {
             Account a = new Account("Mystantt","mystanttLeBg@something.lol","123456789");
             Profile p = new Profile("OrbiterGM", GameRole.GameMaster);
+            a.AddProfile(p);
             Campaign c = new Campaign("Orbiter", "Grosse campagne de fantaisie", new DnD5ERules(), p);
             Profile p2 = new Profile("OrbiterPlayer", GameRole.Player);
             p2.JoinCampaign(c);
